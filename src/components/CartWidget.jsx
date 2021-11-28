55a922024ed92ea9765f361914623d75909f5f32
Zacarias Sanhueza <zacari@gmail.com>
2021-11-28 17:05:09 -0300

    se agrega CartWidget a la navbar

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CartWidget = () => {
+    return (
+        <div>
+             <i className="fas fa-shopping-cart"></i><span class="badge rounded-pill bg-info text-dark">4</span>
+        </div>
+    )
+}
+
+export default CartWidget
+
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../logo.svg";
+import CartWidget from "./CartWidget";
 
 const NavBar =()=>{
 
@@ -35,10 +36,7 @@ return(
           <a className="nav-link disabled">Dino Registro</a>
         </li>
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <CartWidget />
     </div>
   </div>
 </nav>
